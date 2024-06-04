b49cb3d8556e35195e5f552a8f45b0580ddc0c32
Grochu25 <31382364+Grochu25@users.noreply.github.com>
2024-06-04 09:54:06 +0200

    test

--- QuizCreator/App.xaml.cs
+++ QuizCreator/App.xaml.cs
@@ -9,6 +9,7 @@ namespace QuizCreator
     /// <summary>
     /// Interaction logic for App.xaml
     /// </summary>
+    /// duhfduf
     public partial class App : Application
     {
         protected override void OnStartup(StartupEventArgs e)
